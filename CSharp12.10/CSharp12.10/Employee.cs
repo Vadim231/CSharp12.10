fe13759b2fef4cc02d9ef2fc6459a243d723ad93
Vadim231 <40603603+Vadim231@users.noreply.github.com>
2018-11-12 18:43:31 +0300

    Add files via upload

---
+++ CSharp12.10/CSharp12.10/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp12._10
+{
+    enum Vacancies
+    {
+        Manager=1,
+        Boss,
+        Clerk,
+        Salesman
+    };
+    struct Employee
+    {
+        public string name;
+
+        public int age;
+
+        public Vacancies vacancy;
+
+        public int Vacancy
+        {
+            get { return (int)vacancy; }
+            set { vacancy = (Vacancies)value; }
+        }
+
+        public int salary;
+
+        public int employmentDate;
+
+        public Employee(string name, int age, Vacancies vacancy, int salary,int employmentDate)
+        {
+            this.name = name;
+            this.age = age;
+            this.vacancy = vacancy;
+            this.salary = salary;
+            this.employmentDate = employmentDate;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {name}  " +
+                $"Age: {age} " +
+                $"vacancy: {vacancy} " +
+                $"salary: {salary} " +
+                $"EmploymentDate: {employmentDate}");
+        }
+        
+    }
+}
---
+++ CSharp12.10/CSharp12.10/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp12._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество сотрудников: ");
+            int numberOfEmployees = int.Parse(Console.ReadLine());
+
+            Employee[] employees = new Employee[numberOfEmployees];
+            for (int i=0;i< numberOfEmployees; i++)
+            {
+                Console.WriteLine("Введите Имя, Возраст, Вакансию, Зарплату и Дату приема на работу: ");
+                employees[i].name = Console.ReadLine();
+                employees[i].age = int.Parse(Console.ReadLine());
+                employees[i].Vacancy = int.Parse(Console.ReadLine());
+                employees[i].salary = int.Parse(Console.ReadLine());
+                employees[i].employmentDate = int.Parse(Console.ReadLine());
+            }
+
+            SortByFirstName(employees);//сортировка
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine("Сотрудник номер: " + i);
+                Console.WriteLine(employees[i].name);
+                Console.WriteLine(employees[i].age);
+                Console.WriteLine(employees[i].Vacancy);
+                Console.WriteLine(employees[i].salary);
+                Console.WriteLine(employees[i].employmentDate);
+            }
+            
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                if (employees[i].Vacancy == 1)
+                {
+                    Console.WriteLine("Сотрудник номер: " + i);
+                    Console.WriteLine(employees[i].name);
+                    Console.WriteLine(employees[i].age);
+                    Console.WriteLine(employees[i].Vacancy);
+                    Console.WriteLine(employees[i].salary);
+                    Console.WriteLine(employees[i].employmentDate);
+                }
+            }
+            
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                if (employees[i].Vacancy == 2)
+                {
+                    int BossEmp = employees[i].employmentDate;
+                    for (int j = 0; j < numberOfEmployees; j++)
+                    {
+                        if (employees[i].employmentDate > BossEmp)
+                        {
+                            Console.WriteLine("Сотрудник номер: " + i);
+                            Console.WriteLine(employees[i].name);
+                            Console.WriteLine(employees[i].age);
+                            Console.WriteLine(employees[i].Vacancy);
+                            Console.WriteLine(employees[i].salary);
+                            Console.WriteLine(employees[i].employmentDate);
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static public Employee[] SortByFirstName(Employee[] empdata)
+        {
+            int min = 0;
+            Employee temp;
+            for (int i = 0; i < empdata.Length; i++)
+            {
+                for (int j = i + 1; j < empdata.Length; j++)
+                {
+                    if (empdata[i].name.Length < empdata[j].name.Length)
+                    {
+                        min = empdata[i].name.Length;
+                    }
+                    else
+                    {
+                        min = empdata[j].name.Length;
+                    }
+
+                    for (int k = 0; k < min; k++)
+                    {
+                        if (empdata[i].name[k] > empdata[j].name[k])
+                        {
+                            temp = empdata[i];
+                            empdata[i] = empdata[j];
+                            empdata[j] = temp;
+                            break;
+                        }
+                        else if (empdata[i].name[k] == empdata[j].name[k])
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return empdata;
+
+        }
+    }
+}
